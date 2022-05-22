7bf4a590c9a29c850bf3f057b81b21c181fcce23
MrMisterBean <alutter03@gmail.com>
2022-05-21 22:43:01 -0400

    added navigation menu

--- pages/hamburger.js
+++ pages/hamburger.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Link from 'next/link'
 
 export default function Hamburger() {
 
@@ -11,31 +12,52 @@ export default function Hamburger() {
 
   return (
     <div className="flex lg:hidden">
-      <ul className={`${hamburgerOpen ? '' : 'hidden'}`}>
+
+      {/* <ul className={`NAVIGATION-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] ${hamburgerOpen ? 'hidden' : ''}`}>
+        <li className="my-8 uppercase border-b border-gray-400">
+          <a href="/about">About</a>
+        </li>
+        <li className="my-8 uppercase border-b border-gray-400">
+          <a href="/portfolio">Portfolio</a>
+        </li>
+        <li className="my-8 uppercase border-b border-gray-400">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul> */}
+
+      {/* <ul className={`bg-blue-200 w-screen h-screen z-0 p-8 ${hamburgerOpen ? 'hidden' : ''}`}>
         <li>Home</li>
         <li>Menu</li>
         <li>About</li>
         <li>Contact</li>
-      </ul>
-      <div onClick={() => {
+      </ul> */}
+
+      {/* <div onClick={() => {
         setHamburgerOpen(!hamburgerOpen)
         console.log('Clicked hamburger', hamburgerOpen)
-      }}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-6 h-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </div>
-    </div >
+      }}> */}
+      <Link href="/accordian" >
+
+        <a>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+
+        </a>
+      </Link>
+    </div>
+    // </div >
   )
 }
